7944abde380d4912303993b384bb3a10253bff59
Aeolus96 <96811105+Aeolus96@users.noreply.github.com>
2024-02-04 00:47:07 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,85 @@
+# ROS IP Camera Publisher Package
+
+This ROS package streams video from an IP camera using the RTSP protocol, scales down the images, converts it to ROS Image format, and publishes them to a ROS topic.
+
+>!! Higher resolutions will slow down the streaming significantly. !!
+
+Example reference using the default (tested with) streaming resolution:
+
+- Camera -> RTSP stream out
+- 2304x1296x3 = 8,957,952 Bytes/frame x 10 fps ~ 90 MB/s x 8bits ~ 720 Mb/s
+- RTSP stream in -> FFmpeg -> Scaled DOWN into half resolution (default) -> ROS Image out
+- 1152x648x3 = 2,239,488 Bytes/frame x 10 fps ~ 22 MB/s x 8bits ~ 179 Mb/s
+- Total bandwidth used by this stream = 720 Mb/s + 179 Mb/s ~ 900 Mb/s
+
+Most inexpensive/common network setups unless specified are limited to 1000/100/10 Mbit/s per ethernet interface. Hardware acceleration (like NVIDIA CUDA) and additional ethernet hardware can be added to achieve higher resolutions.
+
+## Prerequisites
+
+- ROS Noetic (Robot Operating System): Make sure you have ROS installed (Noetic tested). You can follow the [official ROS installation instructions](http://wiki.ros.org/ROS/Installation).
+- OpenCV: The package relies on OpenCV for image processing. You can install it using:
+
+  ```bash
+  sudo apt-get install ros-${ROS_DISTRO}-cv-bridge
+  ```
+
+- FFmpeg: Ensure that FFmpeg is installed on your system. You can install it using:
+
+  ```bash
+  sudo apt-get install ffmpeg
+  ```
+
+## Package Installation
+
+1. Clone this repository into your ROS workspace:
+
+   ```bash
+   cd ~/<your_ros_workspace>/src
+   git clone https://github.com/Aeolus96/ip_camera_publisher.git
+   ```
+
+2. Build the ROS package:
+
+   ```bash
+   cd ~/<your_ros_workspace>
+   catkin_make
+   ```
+
+3. Source your ROS workspace:
+
+   ```bash
+   source devel/setup.bash
+   ```
+
+## Launching the IP Camera Publisher Node
+
+Use the provided launch file to start the IP camera publisher node. Adjust the launch file parameters according to your IP camera settings.
+
+```bash
+roslaunch ip_camera_publisher ip_camera_publisher.launch
+```
+
+## Launch File Parameters
+
+- **`username`**: Username for RTSP stream authentication (default: "admin").
+- **`password`**: Password for RTSP stream authentication (default: "I%3C3Robots").
+- **`stream_path`**: Path to the RTSP stream on the IP camera (default: "192.168.0.41/h264Preview_01_main").
+- **`image_topic`**: ROS topic to publish the images (default: "/camera/image_raw").
+- **`image_scale`**: Scaling factor for resizing frames (default: 0.5). Keep this between 0.0 and 1.0.
+
+## Node Details
+
+- **Node Name**: `ip_camera_publisher`
+- **Published Topic**: `/camera/image_raw` (adjust based on the `image_topic` parameter)
+
+## Example Launch with Custom Parameters
+
+```bash
+roslaunch ip_camera_publisher ip_camera_publisher.launch username:=<your_username> password:=<your_password> stream_path:=<your_stream_path> image_topic:=<your_image_topic> image_scale:=<0.5>
+```
+
+## License
+
+This project is licensed under the [MIT License](LICENSE).
+
+Feel free to report issues, or make suggestions!
---
+++ scripts/convert.py
@@ -0,0 +1,126 @@
+import rospy
+import json
+import subprocess
+import cv2
+import numpy as np
+from cv_bridge import CvBridge
+from sensor_msgs.msg import Image
+
+
+def get_dimensions(url):
+    """
+    Get the image dimensions using ffprobe
+    """
+    # Get the image dimensions at startup using ffprobe
+    ffprobe_cmd = [
+        "ffprobe",
+        "-v",
+        "error",
+        "-select_streams",
+        "v:0",
+        "-show_entries",
+        "stream=width,height",
+        "-of",
+        "json",
+        url,
+    ]
+    try:
+        rospy.logdebug(f"Running FFprobe: {ffprobe_cmd}")
+        ffprobe_process = subprocess.check_output(ffprobe_cmd, stderr=subprocess.STDOUT)
+        video_info = json.loads(ffprobe_process)
+        width = video_info["streams"][0]["width"]
+        height = video_info["streams"][0]["height"]
+        rospy.logdebug(f"Raw Stream dimensions: {width}x{height}")
+        return width, height
+    except subprocess.CalledProcessError as e:
+        rospy.logerr(f"Error running FFprobe: {e.output}")
+        rospy.loginfo("IP Camera Stream not accessible. Exiting __ip_camera_publisher__ ...")
+        exit()
+
+
+def convert_rtsp_to_opencv(url, width, height):
+    """
+    Streams RTSP video using FFMPEG and converts it to OpenCV image format.
+    Then publishes the image to a ROS topic
+    """
+    # Constructing the ffmpeg command line
+    ffmpeg_cmd = [
+        "ffmpeg",  # ffmpeg command
+        "-nostdin",  # Disabling interaction on standard input in the background process group
+        "-flags",  # Sets flags for low delay for real-time streaming
+        "low_delay",
+        "-rtsp_transport",  # Specifies the RTSP transport protocol as TCP
+        "tcp",
+        "-i",  # Specifies the input URL for the RTSP stream
+        url,
+        "-pix_fmt",  # Sets the pixel format
+        "bgr24",
+        "-an",  # Disables audio recording
+        "-vcodec",  # Sets the video codec
+        "rawvideo",
+        "-vf",  # Adding video filter to scale down the images
+        f"scale={width}:{height}",
+        "-f",  # Specifies the output format
+        "rawvideo",
+        "-",  # Output stream to stdout
+    ]
+
+    # Starting the ffmpeg process
+    ffmpeg_process = subprocess.Popen(ffmpeg_cmd, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
+
+    # Creating an empty numpy array for the raw frame
+    raw_frame = np.empty((height, width, 3), np.uint8)
+    # Make raw frame a buffer that can be overwritten by the ffmpeg process
+    frame_bytes = memoryview(raw_frame).cast("B")
+
+    rospy.loginfo("IP Camera Stream: Starting")
+    # Continuously read frames from the ffmpeg process while ROS is running
+    while ffmpeg_process.poll() is None and not rospy.is_shutdown():
+        try:
+            ffmpeg_process.stdout.readinto(frame_bytes)
+        except ValueError as e:
+            rospy.logerr(f"Error reading frame: {e}")
+            rospy.loginfo("IP Camera Stream not accessible. Exiting __ip_camera_publisher__ ...")
+            break
+        frame = raw_frame.reshape((height, width, 3))  # OpenCV image frame
+        frame = cv2.resize(frame, (1152, 648))  # Scale down to variable resolution?
+
+        image_pub.publish(bridge.cv2_to_imgmsg(frame, "bgr8"))
+
+
+if __name__ == "__main__":
+    rospy.init_node("ip_camera_publisher")
+
+    # Get the RTSP stream URL from the launch params
+    try:
+        rtsp_stream_url = rospy.get_param("rtsp_stream_url")
+        image_topic = rospy.get_param("image_topic")
+        image_scale = rospy.get_param("image_scale")
+        if image_scale > 1.0 or image_scale < 0.0:
+            image_scale = 0.5
+    except KeyError:
+        # If error reading params, exit
+        rospy.logerr("Error: launch parameters error. Exiting __ip_camera_publisher__ ...")
+        exit()
+
+    # Define publisher
+    bridge = CvBridge()
+    image_pub = rospy.Publisher(image_topic, Image, queue_size=1)
+
+    # Default Frame Dimensions - 2304x1296 (lowest clear stream supported by the camera used for testing)
+    width = 2304
+    height = 1296
+
+    # Get the frame dimensions from the RTSP stream
+    stream_width, stream_height = get_dimensions(rtsp_stream_url)
+    if stream_width != width or stream_height != height:
+        rospy.loginfo(f"New Raw Frame dimensions: {stream_width}x{stream_height}")
+        width = stream_width
+        height = stream_height
+
+    # Scale down the frame dimensions
+    width = int(width * image_scale)
+    height = int(height * image_scale)
+
+    # Convert RTSP stream to OpenCV image and publish
+    convert_rtsp_to_opencv(rtsp_stream_url, width, height)
